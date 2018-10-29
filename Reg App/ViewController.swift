aace89972ec21f1bb23f605cddec7e5b929edc1a
AhsanIqbal <syed_ahsan_iqbal@yahoo.com>
2018-10-29 17:50:15 +0500

    Initial Commit

---
+++ Reg App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reg App
+//
+//  Created by Ahsan Iqbal on 29/10/2018.
+//  Copyright © 2018 SelfIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
